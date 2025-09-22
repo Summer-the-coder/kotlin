a2f17d187cc922db4b8912401762a41e1361c660
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-09-22 12:33:46 +0300

    [Tests] KQA-2443, KT-48670, KT-70052: chains of calls of inline functions
    With typeOf and different combinations of reified/non-reified TPs

--- native/native.tests/codegen-box/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestGenerated.java
+++ native/native.tests/codegen-box/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestGenerated.java
@@ -63888,7 +63888,19 @@ public class FirNativeCodegenBoxTestGenerated extends AbstractNativeCodegenBoxTe
       public void testFunWithReifiedTypeParameterContextParameterAndExtensionReceiverInsideClass() {
         runTest("compiler/testData/codegen/box/typeErasure/funWithReifiedTypeParameterContextParameterAndExtensionReceiverInsideClass.kt");
       }
-      
+
+      @Test
+      @TestMetadata("funWithReifiedTypeParameterInlinesFunWithThreeTypeParameters.kt")
+      public void testFunWithReifiedTypeParameterInlinesFunWithThreeTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/funWithReifiedTypeParameterInlinesFunWithThreeTypeParameters.kt");
+      }
+
+      @Test
+      @TestMetadata("funWithReifiedTypeParameterInlinesFunWithTwoTypeParameters.kt")
+      public void testFunWithReifiedTypeParameterInlinesFunWithTwoTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/funWithReifiedTypeParameterInlinesFunWithTwoTypeParameters.kt");
+      }
+
       @Test
       @TestMetadata("funWithReifiedTypeParameterInsideClass.kt")
       public void testFunWithReifiedTypeParameterInsideClass() {
@@ -63995,6 +64007,30 @@ public class FirNativeCodegenBoxTestGenerated extends AbstractNativeCodegenBoxTe
       @TestMetadata("outerClassWithTypeParameterWithTwoUpperBounds.kt")
       public void testOuterClassWithTypeParameterWithTwoUpperBounds() {
         runTest("compiler/testData/codegen/box/typeErasure/outerClassWithTypeParameterWithTwoUpperBounds.kt");
+      }
+
+      @Test
+      @TestMetadata("twoFunWithReifiedTypeParametersInlineFunWithReifiedTypeParameters.kt")
+      public void testTwoFunWithReifiedTypeParametersInlineFunWithReifiedTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/twoFunWithReifiedTypeParametersInlineFunWithReifiedTypeParameters.kt");
+      }
+
+      @Test
+      @TestMetadata("twoFunWithReifiedTypeParametersInlineFunWithTwoTypeParameters.kt")
+      public void testTwoFunWithReifiedTypeParametersInlineFunWithTwoTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/twoFunWithReifiedTypeParametersInlineFunWithTwoTypeParameters.kt");
+      }
+
+      @Test
+      @TestMetadata("twoFunWithTypeParametersInlineFunWithReifiedTypeParameters.kt")
+      public void testTwoFunWithTypeParametersInlineFunWithReifiedTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/twoFunWithTypeParametersInlineFunWithReifiedTypeParameters.kt");
+      }
+
+      @Test
+      @TestMetadata("twoFunWithTypeParametersWithUpperBoundsInlineFunWithReifiedTypeParameters.kt")
+      public void testTwoFunWithTypeParametersWithUpperBoundsInlineFunWithReifiedTypeParameters() {
+        runTest("compiler/testData/codegen/box/typeErasure/twoFunWithTypeParametersWithUpperBoundsInlineFunWithReifiedTypeParameters.kt");
       }
     }
 
