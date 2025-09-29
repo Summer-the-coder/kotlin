36a70d4fc09afb6112f872263c2209bb76879859
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2025-09-29 10:47:22 +0200

    [BTA] Move KotlinToolchainV1Adapter.kt to correct package path
    # Conflicts:
    #	compiler/build-tools/kotlin-build-tools-compat/src/main/kotlin/KotlinToolchainV1Adapter.kt
    #	compiler/build-tools/kotlin-build-tools-compat/src/main/kotlin/KotlinToolchainsV1Adapter.kt
    #	compiler/build-tools/kotlin-build-tools-compat/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/compat/KotlinToolchainV1Adapter.kt

--- compiler/build-tools/kotlin-build-tools-compat/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/compat/KotlinToolchainsV1Adapter.kt
+++ compiler/build-tools/kotlin-build-tools-compat/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/compat/KotlinToolchainsV1Adapter.kt
@@ -72,7 +72,10 @@ public class KotlinToolchainsV1Adapter(
     }
 }
 
-private class JvmClasspathSnapshottingOperationV1Adapter(val compilationService: CompilationService, val classpathEntry: Path) :
+private class JvmClasspathSnapshottingOperationV1Adapter(
+    val compilationService: CompilationService,
+    val classpathEntry: Path,
+) :
     BuildOperationImpl<ClasspathEntrySnapshot>(), JvmClasspathSnapshottingOperation {
     private val options: Options = Options(JvmClasspathSnapshottingOperation::class)
 
@@ -267,7 +270,8 @@ internal fun List<String>.fixForFirCheck(): List<String> {
 private interface ExecutionPolicyV1Adapter {
     val strategyConfiguration: CompilerExecutionStrategyConfiguration
 
-    class InProcess(override val strategyConfiguration: CompilerExecutionStrategyConfiguration) : ExecutionPolicyV1Adapter,
+    class InProcess(override val strategyConfiguration: CompilerExecutionStrategyConfiguration) :
+        ExecutionPolicyV1Adapter,
         ExecutionPolicy.InProcess
 
     class WithDaemon(private val compilationService: CompilationService) : ExecutionPolicyV1Adapter,
