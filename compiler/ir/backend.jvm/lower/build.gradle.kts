9f357f67989f74aae8f51243ebcbca7ec4eb833d
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-09-29 21:49:50 +0200

    JVM: refactor phase creation and checking prerequisites
     #KT-67463 Fixed

--- compiler/ir/backend.jvm/lower/build.gradle.kts
+++ compiler/ir/backend.jvm/lower/build.gradle.kts
@@ -9,13 +9,13 @@ dependencies {
     api(project(":compiler:ir.inline"))
     api(project(":compiler:backend.jvm"))
     compileOnly(intellijCore())
+
+    testImplementation(kotlinTest("junit"))
 }
 
 optInToUnsafeDuringIrConstructionAPI()
 
 sourceSets {
-    "main" {
-        projectDefault()
-    }
-    "test" {}
+    "main" { projectDefault() }
+    "test" { projectDefault() }
 }
