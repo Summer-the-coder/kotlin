63cf5e0076c6c4358a1d6575b3d1c598330c9fc5
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-09-30 16:36:59 +0200

    fixup! [Tests] Add tests for type parameters captured by synthetic accessors

--- compiler/testData/klib/syntheticAccessors/privateMember/singleFile/leakingCapturedTypeParameter.kt
+++ compiler/testData/klib/syntheticAccessors/privateMember/singleFile/leakingCapturedTypeParameter.kt
@@ -12,6 +12,13 @@ class A<T> {
         internal inline fun callBar(x: T, y: S) = bar(x, y)
     }
 
+    inner class C<S> private constructor(val x: S) {
+        @Suppress("INVISIBLE_REFERENCE")
+        internal inline fun copy() = C<Int>(42)
+    }
+    @Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+    internal inline fun getC(s: String) = C<String>(s)
+
     companion object Companion {
         private fun bar(x: Any) = x
         internal inline fun callBar(x: Any) = bar(x)
@@ -29,7 +36,9 @@ fun box(): String {
     res += A<String>().callBaz("OK2 ", "NO2 ").first
     res += A<String>().callBaz("NO3 ", "OK3 ").second
     res += A<String>().B<String>().callBar("OK4 ", "NO4 ").first
-    res += A<String>().B<String>().callBar("NO5", "OK5").second
-    if (res != "OK1 OK2 OK3 OK4 OK5") return res
+    res += A<String>().B<String>().callBar("NO5", "OK5 ").second
+    res += A<String>().getC("OK6 ").x
+    res += if (A<String>().getC("OK").copy().x == 42) "OK7" else "NO7"
+    if (res != "OK1 OK2 OK3 OK4 OK5 OK6 OK7") return res
     else return "OK"
 }
