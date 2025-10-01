cf366f9d37ce2a66210a0e473f3e2e213af36d45
Andrii Rublov <andrii.rublov@jetbrains.com>
2025-10-01 11:56:45 +0200

    [K/JS]: ANTLR: Fix validation visitor nullability

--- js/js.parser/src/org/jetbrains/kotlin/js/parser/antlr/AntlrJsValidationVisitor.kt
+++ js/js.parser/src/org/jetbrains/kotlin/js/parser/antlr/AntlrJsValidationVisitor.kt
@@ -9,14 +9,14 @@ import com.google.gwt.dev.js.rhino.ErrorReporter
 import org.antlr.v4.runtime.ParserRuleContext
 import org.jetbrains.kotlin.js.parser.antlr.generated.JavaScriptParser
 
-class AntlrJsValidationVisitor(private val reporter: ErrorReporter) : AntlrJsBaseVisitor<ParserRuleContext>() {
-    override fun visitAssignmentExpression(ctx: JavaScriptParser.AssignmentExpressionContext): ParserRuleContext {
+class AntlrJsValidationVisitor(private val reporter: ErrorReporter) : AntlrJsBaseVisitor<ParserRuleContext?>() {
+    override fun visitAssignmentExpression(ctx: JavaScriptParser.AssignmentExpressionContext): ParserRuleContext? {
         if (ctx.lhs is JavaScriptParser.ThisExpressionContext)
             reporter.error("Invalid assignment left-hand side.", ctx.lhs.startPosition, ctx.lhs.stopPosition)
         return super.visitAssignmentExpression(ctx)
     }
 
-    override fun visitAssignmentOperatorExpression(ctx: JavaScriptParser.AssignmentOperatorExpressionContext): ParserRuleContext {
+    override fun visitAssignmentOperatorExpression(ctx: JavaScriptParser.AssignmentOperatorExpressionContext): ParserRuleContext? {
         if (ctx.lhs is JavaScriptParser.ThisExpressionContext)
             reporter.error("Invalid assignment left-hand side.", ctx.lhs.startPosition, ctx.lhs.stopPosition)
         return super.visitAssignmentOperatorExpression(ctx)
